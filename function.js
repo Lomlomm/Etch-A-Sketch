4c3e2807c0d882249351e12cf7e1ea85e1efa477
vanessa_arellano <vanessaserna60@gmail.com>
2023-01-26 12:09:13 -0600

    Add draw method with click and mouseover

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", function(){
-    createBoard(32)
+    createBoard(16)
    
 })
 
 function createBoard(size){
     let grid = document.querySelector('.grid')
+    let clicked = false
     grid.style.display = "grid"
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -13,14 +14,31 @@ function createBoard(size){
         div = document.createElement('div'); 
         div.style.border = '1px solid #dbd7d7'
         div.classList = 'casilla'
+        div.addEventListener('click', function(){
+            if (clicked == true){
+                clicked = false
+            }
+            else{
+                clicked = true
+                this.style.backgroundColor = 'black';
+            }
+            
+        }); 
         div.addEventListener('mouseover', function(){
-            this.style.backgroundColor = 'black'
+            if (clicked == true){
+                this.style.backgroundColor = 'black'
+            }
+            
         })
+
         grid.insertAdjacentElement('beforeend', div)
         
     }
 }
 
+function draw(div){
+    
+}
 
 // grid-template-columns: 60px 60px;
 // grid-template-columns: repeat(2, 60px);
